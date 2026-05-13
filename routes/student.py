62cec39dc09cc26a84340bfee4f47da589a320f2
pl99player99 <player99pl99@gmail.com>
2026-05-13 01:23:20 +0100

    er

--- routes/student.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-student_bp = Blueprint('student', __name__)
-
-@student_bp.route('/dashboard')
-def dashboard():
-    return render_template('student/dashboard.html')
--- src/components/ui/skeleton.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { cn } from "@/lib/utils";
-function Skeleton({ className, ...props }) {
-    return (_jsx("div", { className: cn("animate-pulse rounded-md bg-zinc-900/10 dark:bg-zinc-50/10", className), ...props }));
-}
-export { Skeleton };
